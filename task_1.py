f816d9a068718836cf0330c0f743fb6a3bb42378
mbibaeva <melisanushk@gmail.com>
2017-09-12 01:02:13 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,8 +0,0 @@
-
-def fibonacci(n):
-    mem = {0: 1, 1: 1}
-    if n < 0 or type(n) is float:
-        return
-    if n not in mem:
-        mem[n] = fibonacci(n-1) + fibonacci(n-2)
-    return mem[n]
